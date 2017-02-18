32e60ce4c0edca8ba32a408450af8dee63849b67
rohits2509 <rohits2509@gmail.com>
2017-02-18 10:45:42 -0800

    Tested with hard coded values

--- colorGame.js
+++ colorGame.js
@@ -1,15 +1,32 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 0, 0)",
-	"rgb(255, 0, 0)",
-	"rgb(255, 0, 0)",
-	"rgb(255, 0, 0)",
-	"rgb(255, 0, 0)"
+	var colors = [
+		"rgb(255, 0, 0)",
+		"rgb(255, 255, 0)",
+		"rgb(0, 255, 0)",
+		"rgb(0, 255, 255)",
+		"rgb(0, 0, 255)",
+		"rgb(255, 0, 255)"
 	];
 
-var squares = document.querySelectorAll(".square");
+	var colorDisplay = document.getElementById("display")
+	var squares = document.querySelectorAll(".square");
 
-for(var i = 0; i < squares.length; i++){
+	var pickedColor = colors[3];
+	colorDisplay.textContent = pickedColor;
+
+	for(var i = 0; i < squares.length; i++){
 	squares[i].style.background = colors[i];
+	
+	
+	//add event listener to squares
+	squares[i].addEventListener("click", function(){
+		//grab color of clciked square
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor){
+			alert("correct");
+		} else{
+			alert("Wrong!");
+		}
 
+	});
 }
