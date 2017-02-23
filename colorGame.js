7f6427184834909d1a114b591d24751e1681f1f4
rohits2509 <rohits2509@gmail.com>
2017-02-22 22:34:22 -0800

    refactored code

--- colorGame.js
+++ colorGame.js
@@ -22,7 +22,7 @@ function setupModeButtons(){
 		modeButtons[i].addEventListener("click", function(){
 			modeButtons[0].classList.remove("selected");
 			modeButtons[1].classList.remove("selected");
-			this.classList.add("selected");
+			modeButtons[i].classList.add("selected");
 			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
 			reset();
 		});
